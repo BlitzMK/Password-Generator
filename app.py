6945ca2472eb338c46241e2674d104f0e8777e1d
Samuel Chandler <sec180005@utdallas.edu>
2023-02-03 12:29:56 -0600

    set inputs for index

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 @app.route("/", methods=["POST","GET"])
 def index():
     if request.method == "POST":
-        return redirect(url_for("generatedPassword", nam = request.form["nm"]))
+        return redirect(url_for("generatedPassword", len = request.form["length"]))
     else:
         return render_template('index.html')
 
-@app.route("/generatedPassword/<nam>", methods = ["POST","GET"])
-def generatedPassword(nam):
-    return render_template("generatedPassword.html", password = nam )
+@app.route("/generatedPassword/<len>", methods = ["POST","GET"])
+def generatedPassword(len):
+    return render_template("generatedPassword.html", password = len )
 
 if __name__ == "__main__":
     app.run(debug=True)
