f13a067b589a2d25be97f99865f1eb8af6392598
Samuel Chandler <sec180005@utdallas.edu>
2023-02-03 11:55:21 -0600

    basic input and output

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, session
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=["POST","GET"])
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        return redirect(url_for("generatedPassword", nam = request.form["nm"]))
+    else:
+        return render_template('index.html')
+
+@app.route("/generatedPassword/<nam>", methods = ["POST","GET"])
+def generatedPassword(nam):
+    return render_template("generatedPassword.html", password = nam )
 
 if __name__ == "__main__":
     app.run(debug=True)
